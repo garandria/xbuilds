c82f5caef507efc1ef28b937122f2edd9e0f5947
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2023-02-13 17:26:52 +0100

    busybox: scripts: add script that generates N distinct randconfigs
    To compare the configuration (.config) files between each other, this script
    does a checksum of each one of them (after their generation) with `cksum` and
    saves it in a file (.db). Hence the checking is verifying if the checksum of the
    lastly generated .config is in .db.
    
    Since the date of the generation of the .config is written on Line 4 of each
    file, the script removes Line 4 of each .config then does a checksum of the file
    without this line.

---
+++ busybox/scripts/generate_configs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DB_FILE=.db
+CONFIG=.config
+OUT_DIR=randconfig_generated
+NB_CONFIGS=$1
+
+touch $DB_FILE
+mkdir -p $OUT_DIR
+
+i=1
+while [[ $i -le $NB_CONFIGS ]] ;
+do
+    make randconfig >> /dev/null 2>&1
+    CKSUM=$(sed '4d' $CONFIG | cksum)
+    echo "$CKSUM"
+    if [[ ! $(grep "$CKSUM" $DB_FILE) ]] ; then
+        echo "$CKSUM" >> $DB_FILE
+        ii=$(printf "%04d" $i)
+        mv $CONFIG $OUT_DIR/${ii}_randconfig
+        i=$[$i+1]
+    fi
+done
+
+rm $DB_FILE
